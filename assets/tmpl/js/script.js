df731f27cbb48c238f944980886282176939ef75
viteknike <vitek.makarenskii99@gmail.com>
2020-02-14 14:56:05 +0300

    /

---
+++ assets/tmpl/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $('#festCarousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                
+                items:2
+            },
+            900:{
+                items:2
+            },
+        }
+    })
+    $('#spiakerCarousel').owlCarousel({
+        loop:true,
+        margin:30,
+        dots:true,
+        responsive:{
+            0:{
+                items:2,
+                margin:10
+            },
+            600:{
+                items:2,
+                margin:10,
+            },
+            900:{
+                items:4,
+                margin:10,
+            }
+        }
+    })
+
+});
